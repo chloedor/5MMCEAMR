5882377ad31e2a13bf7c827068fe50e66b8a06e9
Chloé Dorchêne <Chloe.Dorchene@grenoble-inp.org>
2023-01-17 16:28:21 +0100

    nettoyage du code

--- TP/tp-simd/tp2_src_etd/conv-sse-a-trou.c
+++ TP/tp-simd/tp2_src_etd/conv-sse-a-trou.c
@@ -30,8 +30,6 @@
 #include <stdalign.h> 
 #include <xmmintrin.h>
 
-uint8_t test = 0; 
-
 /* Fonction d'affichage de variables de __m128x sous divers formats */
 void p128_x(__m128 fl, __m128i in, __m128i sh, __m128i ch)
 {
@@ -67,14 +65,7 @@ void YCrCb_to_ARGB(uint8_t *YCrCb_MCU[3], uint32_t *RGB_MCU, uint32_t nb_MCU_H, 
    __m128i Rc, Gc, Bc;
 
    /* On définit le tableau de zéro dont on va avoir besoin */
-   /*
-    * Instructions à utiliser
-    *     _mm_set... 
-    */
    const __m128i v0         = _mm_set1_epi32(0);
-   if (test){
-      p128_x(Rf, v0, v0, v0);
-   }
 
    MCU_Y  = YCrCb_MCU[0];
    MCU_Cb = YCrCb_MCU[1];
@@ -85,45 +76,17 @@ void YCrCb_to_ARGB(uint8_t *YCrCb_MCU[3], uint32_t *RGB_MCU, uint32_t nb_MCU_H, 
          /* On travaille à présent sur des vecteurs de 4 éléments d'un coup */
          index = i * (8 * nb_MCU_H) + j;
 
-         /*
-          * Chargement des macro-blocs:
-          * instruction à utiliser
-          *     _mm_set...
-          */
-         if(test) {
-            printf("%hhu\n%hhu\n%hhu\n%hhu\n", MCU_Cr [index + 0], MCU_Cr [index + 1], MCU_Cr [index + 2], MCU_Cr [index + 3]);
-         }
+         /* Chargement des macro-blocs: */
          Y  = _mm_setr_ps(MCU_Y [index + 0], MCU_Y [index + 1], MCU_Y [index + 2], MCU_Y [index + 3]);
          Cb = _mm_setr_ps(MCU_Cb [index + 0], MCU_Cb [index + 1], MCU_Cb [index + 2], MCU_Cb [index + 3]);
          Cr = _mm_setr_ps(MCU_Cr [index + 0], MCU_Cr [index + 1], MCU_Cr [index + 2], MCU_Cr [index + 3]);
-         if (test) {
-            p128_x(Y, v0, v0, v0);
-            p128_x(Cb, v0, v0, v0);            
-            p128_x(Cr, v0, v0, v0);
-         }
 
 
-         /*
-          * Calcul de la conversion YCrCb vers RGB:
-          *    aucune instruction sse explicite
-          */
+         /* Calcul de la conversion YCrCb vers RGB: */
 
          Rf = (Cr - 128.0f) * 1.402f + Y;
          Bf = (Cb - 128.0f) * 1.7772f + Y;
          Gf = Y - (Cb - 128.0f) * 0.381834f - (Cr - 128.0f)*0.71414f;
-
-         /*
-         Rf = _mm_add_ps(_mm_mul_ps(_mm_sub_ps(Cr, _mm_set1_ps(128.0)),  _mm_set1_ps(1.402f)), Y);
-         Bf = _mm_add_ps(_mm_mul_ps(_mm_sub_ps(Cb, _mm_set1_ps(128.0)),  _mm_set1_ps(1.7772f)), Y);
-         Gf = _mm_sub_ps(_mm_sub_ps(Y, _mm_mul_ps(_mm_sub_ps(Cb, _mm_set1_ps(128)),  _mm_set1_ps(0.381834f))), _mm_mul_ps(_mm_sub_ps(Cr, _mm_set1_ps(128)),  _mm_set1_ps(0.71414f)));
-         */
-
-         if (test) {
-            p128_x(Rf, v0, v0, v0);
-            p128_x(Gf, v0, v0, v0);            
-            p128_x(Bf, v0, v0, v0);
-         }
-
 
          /*
           * Conversion en entier avec calcul de la saturation:
@@ -131,9 +94,6 @@ void YCrCb_to_ARGB(uint8_t *YCrCb_MCU[3], uint32_t *RGB_MCU, uint32_t nb_MCU_H, 
           *    conversion vecteur flottants 32 bits vers vecteur entiers 32 bits
           *    conversion vecteur entiers 32 bits vers vecteur entiers 16 bits non signé en saturant
           *    conversion vecteur entiers 16 bits vers vecteur entiers 8 bits non signé en saturant
-          * instructions à utiliser
-          *    _mm_cvt...
-          *    _mm_pack...
           */
          R = _mm_cvtps_epi32(Rf);
          G = _mm_cvtps_epi32(Gf);
@@ -147,35 +107,14 @@ void YCrCb_to_ARGB(uint8_t *YCrCb_MCU[3], uint32_t *RGB_MCU, uint32_t nb_MCU_H, 
          Gc = _mm_packus_epi16(Gs, Gs);
          Bc = _mm_packus_epi16(Bs, Bs);
 
-         if (test) {
-            p128_x(Cr, R, Rs, Rc);
-            p128_x(Cr, G, Gs, Gc);            
-            p128_x(Cr, B, Bs, Bc);
-         }
-
-         /*
-          * Transposition de la matrice d'octets, brillant !
-          * instructions à utiliser
-          *    _mm_unpack...
-          */
+         /* Transposition de la matrice d'octets, brillant ! */
          __m128i tmp0, tmp1;
          tmp0 = _mm_unpacklo_epi8(Rc, v0);
          tmp1 = _mm_unpacklo_epi8(Bc, Gc);
          ARGB = _mm_unpacklo_epi16(tmp1, tmp0);
 
-         if (test) {
-            p128_x(Cr, tmp0, tmp0, tmp0);
-            p128_x(Cr, tmp0, tmp1, tmp1);
-            p128_x(Cr, ARGB, ARGB, ARGB);
-         }
-
-         /*
-          * Écriture du résultat en mémoire
-          * instruction à utiliser
-          * _mm_store...
-          */
+         /* Écriture du résultat en mémoire */
          _mm_storeu_si128((__m128i *)&RGB_MCU[index], ARGB);
-         test = 0;
       }
    }
 }
